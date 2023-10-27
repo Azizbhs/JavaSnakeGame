fad9d1ca614676101336762f04c6b9f28ca055cc
Aziz <bhsaziz408@gmail.com>
2023-10-27 22:18:16 +0200

    Interface

--- Frame.java
+++ Frame.java
@@ -1,5 +1,13 @@
 import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class  Frame extends JFrame{
-    
+public class Frame extends JFrame implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
 }
