f526dda8684d2cc60236ccd725a04680a8e13a5f
Aziz <bhsaziz408@gmail.com>
2023-10-27 22:22:46 +0200

    we start

--- Panel.java
+++ Panel.java
@@ -1,5 +1,13 @@
 import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Panel extends JPanel{
+public class Panel extends JPanel implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
     
 }
