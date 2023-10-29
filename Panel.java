6020d270a92534a80a80e444f8c92b4725137751
Aziz <bhsaziz408@gmail.com>
2023-10-29 23:41:30 +0100

    Panel

--- Panel.java
+++ Panel.java
@@ -1,17 +1,41 @@
 import javax.swing.JPanel;
+
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Panel extends JPanel implements ActionListener{
     
-    GamePanel(){
+    Panel(){
         
     }
+    
+     public void startGame(){
 
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    
+    public void move(){
+
+    }
+
+    public void checkApple(){
+
+    }
+    
+    public void checkCollisions(){
+
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
+        
     
 }
