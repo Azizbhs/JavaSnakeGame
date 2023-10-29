a9da33f000edb71496f275c0026333b697d57ac6
Aziz <bhsaziz408@gmail.com>
2023-10-29 23:40:08 +0100

    "Frame"

--- Frame.java
+++ Frame.java
@@ -3,8 +3,16 @@ import javax.swing.JFrame;
 
 public class Frame extends JFrame{
 
-    GameFrame(){
-        
-    }
+ Frame(){
+    
+    this.add(new Panel());
+    this.setTitle("Snake");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setResizable(false);
+    this.pack();
+    this.setVisible(true);
+    this.setLocationRelativeTo(null);
+
+ }
 
 }
