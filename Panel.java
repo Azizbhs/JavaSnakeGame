c0ae2b882decbd8ab07cf1d875c7b267403918b7
Aziz <bhsaziz408@gmail.com>
2023-11-01 23:39:35 +0100

    Movement

--- Panel.java
+++ Panel.java
@@ -60,6 +60,25 @@ public class Panel extends JPanel implements ActionListener{
     }
     
     public void move(){
+        for(int i = bodyparts; i>0; i--){
+            x[i]= x[i-1];
+            y[i]=y[i-1];
+        }
+
+        switch(direction){
+            case 'U':
+               y[0] = y[0] - unit_size;
+                break;
+            case 'D':
+               y[0] = y[0] + unit_size;
+                break;
+            case 'L':
+               x[0] = x[0] - unit_size;
+                break;
+            case 'R':
+               x[0] = x[0] + unit_size;
+                break;
+        }
 
     }
 
